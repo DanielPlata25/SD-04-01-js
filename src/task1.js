5118f12782446bb8f6e18c4d94f1b70a6e682e3f
Daniel Plata <daniel.plata0425@gmail.com>
2025-12-17 16:28:46 -0600

    Subida de trabajo

--- src/task1.js
+++ src/task1.js
@@ -1,12 +1,14 @@
-const arr = []
+const arr = [];
 
 // Escribe tu codigo por debajo de esta linea
 
-
+for (let index = 1; index <= 20; index++) {
+  arr.push(index);
+}
 
 // Escribe tu codigo por encima de esta linea
 
 //No tocar esta funcion
-export function taskArr(){
+export function taskArr() {
   return arr;
 }
--- src/task6.js
+++ src/task6.js
@@ -1,8 +1,10 @@
-const arr1 = ["hello"]
-const arr2 = ["world"]
+const arr1 = ["hello"];
+const arr2 = ["world"];
 
 // Type your code below this line!
 
+const arr3 = arr1.concat(arr2);
 
+console.log(arr3);
 
 // Type your code above this line!
